8aecd189d5b2b843665f859e2fe0f50c911d45d0
davidmacf <david@macf.co.uk>
2021-03-05 19:00:52 +0000

    Update search.php

--- search.php
+++ search.php
@@ -1,4 +1,5 @@
 <?php
+// conditional statment to load the search functionality
 if ($client->variation('search-bar', $user, false)) {
   // application code to show the feature
   require 'search.php';
